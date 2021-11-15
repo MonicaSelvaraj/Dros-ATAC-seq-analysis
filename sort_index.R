bb965239eaafe340a87775e5ed407394b10a3551
selvaraj <selvaraj@tak.wi.mit.edu>
2021-11-15 15:49:39 -0500

    script to sort and index aligned reads bam file

---
+++ sort_index.R
@@ -0,0 +1,12 @@
+#This script sorts and indexes mapped ATAC reads
+
+library(Rsamtools)
+
+#make an output folder for sorted bam. CHECK THIS - it ended up one index up
+sortedBAM <- "/lab/solexa_lehmann/Dros-ATAC-seq-analysis/Sorted_ATAC.bam"
+
+#sort bam file
+sortBam(outBAM, gsub("\\.bam", "", basename(sortedBAM)))
+
+#index bam file
+indexBam(sortedBAM)
