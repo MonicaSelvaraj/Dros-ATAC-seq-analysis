f4756ee3dedf6be7a4669d2052d0a98c082292a4
selvaraj <selvaraj@tak.wi.mit.edu>
2021-11-29 11:23:20 -0500

    updating scripts

--- num_mapped_reads.R
+++ num_mapped_reads.R
@@ -1,5 +1,5 @@
 #This script counts the number of mapped reads
 library(Rsubread)
-sortedBAM <- "/lab/solexa_lehmann/Dros-ATAC-seq-analysis/Sorted_ATAC.bam"
+sortedBAM <- "/lab/solexa_lehmann/Dros-ATAC-seq-analysis/noDups_noMito.bam"
 pmapped <- propmapped(sortedBAM)
 pmapped
--- plot_insert_sizes.R
+++ plot_insert_sizes.R
@@ -3,7 +3,7 @@ library(magrittr)
 library(dplyr)
 library(ggplot2)
 
-sortedBAM <- "/lab/solexa_lehmann/Dros-ATAC-seq-analysis/Sorted_ATAC.bam"
+sortedBAM <- "/lab/solexa_lehmann/Dros-ATAC-seq-analysis/Sorted_noDups_noMito_ATAC.bam"
 
 #retrieve insert sizes
 atacReads <- readGAlignmentPairs(sortedBAM, param = ScanBamParam(mapqFilter = 1, 
@@ -11,24 +11,41 @@ atacReads <- readGAlignmentPairs(sortedBAM, param = ScanBamParam(mapqFilter = 1,
                                                                                                                                     "mapq", "isize")))
 atacReads_read1 <- GenomicAlignments::first(atacReads)
 insertSizes <- abs(elementMetadata(atacReads_read1)$isize)
-#head(insertSizes)
+#print(insertSizes)
+
+#loop to count number of fragments below and above 150
+#count_less <- 0
+#count_more <- 0
+#total <- length(insertSizes)
+#for (i in insertSizes){
+#	if (i <= 150){ 
+#		count_less = count_less + 1
+#	}
+#	else{ 
+#		count_more = count_more + 1
+#	}
+#}
+#print(count_less/total)
+#print(count_more/total)
+		
+
 #this is somewhat opaque to me but ... these numbers are matching, so hopefully the paired reads are the same length?
 
 #plot fragment length with ggplot
-fragLenPlot <- table(insertSizes) %>% data.frame %>% rename(InsertSize = insertSizes, 
-                                                            Count = Freq) %>% mutate(InsertSize = as.numeric(as.vector(InsertSize)), 
-                                                                                     Count = as.numeric(as.vector(Count))) %>% ggplot(aes(x = InsertSize, y = Count)) + 
-  geom_line()
+#fragLenPlot <- table(insertSizes) %>% data.frame %>% rename(InsertSize = insertSizes, 
+#                                                            Count = Freq) %>% mutate(InsertSize = as.numeric(as.vector(InsertSize)), 
+#                                                                                     Count = as.numeric(as.vector(Count))) %>% ggplot(aes(x = InsertSize, y = Count)) + 
+#  geom_line()
 
-pdf(file="/lab/solexa_lehmann/Dros-ATAC-seq-analysis/fragment_length_plot_preprocessing.pdf", width = 5, height = 5)
+#pdf(file="/lab/solexa_lehmann/Dros-ATAC-seq-analysis/fragment_length_plot_postprocessing_noMito.pdf", width = 5, height = 5)
 
-fragLenPlot + theme_bw()
+#fragLenPlot + theme_bw()
 #looks very monosome-ish
 
-dev.off()
+#dev.off()
 
-pdf(file="/lab/solexa_lehmann/Dros-ATAC-seq-analysis/fragment_length_log_plot_preprocessing.pdf", width = 5, height = 5)
+#pdf(file="/lab/solexa_lehmann/Dros-ATAC-seq-analysis/fragment_length_log_plot_postprocessing_noMito.pdf", width = 5, height = 5)
 #plot on a log scale
-fragLenPlot + scale_y_continuous(trans = "log2") + theme_bw()
+#fragLenPlot + scale_y_continuous(trans = "log2") + theme_bw()
 #still very monosome-ish
-dev.off()
+#dev.off()
