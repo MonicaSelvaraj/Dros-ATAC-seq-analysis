6d4e62fba8211ff6a48e434059b8ad7df531d44c
selvaraj <selvaraj@tak.wi.mit.edu>
2021-12-09 23:58:59 -0500

    no modifications

--- atac_seq_full.R
+++ atac_seq_full.R
@@ -21,11 +21,11 @@ library(dplyr)
 #Drosophila genome file path: "/nfs/genomes/d.melanogaster_aug_14/fasta_canonical_whole_genome/dm6_can.fa"
 
 #Read genome file path
-genome <- "/nfs/genomes/d.melanogaster_aug_14/fasta_canonical_whole_genome/dm6_can.fa"
+#genome <- "/lab/solexa_lehmann/Dros-ATAC-seq-analysis/Greenleaf_original_ATAC_data/hg19_Genome.fa"
 #set working file path
 indexForSubread <- ("/lab/solexa_lehmann/Dros-ATAC-seq-analysis")
 #build index
-buildindex(indexForSubread, genome_hum, indexSplit = FALSE)
+#buildindex(indexForSubread, genome, indexSplit = FALSE)
 
 #---------------------------------------------------------------------------------------
 #STEP 2: Align ATAC-seq paired end reads to indexed reference genome. 
@@ -35,15 +35,19 @@ buildindex(indexForSubread, genome_hum, indexSplit = FALSE)
 #TODO: Change file path to forward and reverse reads here.
 #The data is usually in the following home folder: "/lab/solexa_public/LEHMANN/"
 
-read1 <- "/lab/solexa_public/LEHMANN/211123_WIGTC-MISEQ_K56TW/FASTQ/trimmed/L460_2_S1_L001_R1_001.fastq.gz"
-read2 <- "/lab/solexa_public/LEHMANN/211123_WIGTC-MISEQ_K56TW/FASTQ/trimmed/L460_2_S1_L001_R2_001.fastq.gz"
+#Original paper human reads
+#read1 <- "/lab/solexa_lehmann/Dros-ATAC-seq-analysis/Greenleaf_original_ATAC_data/SRR891269/SRR891269_1.fastq.gz"
+#read2 <- "/lab/solexa_lehmann/Dros-ATAC-seq-analysis/Greenleaf_original_ATAC_data/SRR891269/SRR891269_2.fastq.gz"
+
+#read1 <- "/lab/solexa_public/LEHMANN/211123_WIGTC-MISEQ_K56TW/FASTQ/trimmed/L460_2_S1_L001_R1_001.fastq.gz"
+#read2 <- "/lab/solexa_public/LEHMANN/211123_WIGTC-MISEQ_K56TW/FASTQ/trimmed/L460_2_S1_L001_R2_001.fastq.gz"
 
 #make an output path
 outBAM <- "/lab/solexa_lehmann/Dros-ATAC-seq-analysis/ATAC.bam"
 
 #align reads to generate bam file
-align(indexForSubread, readfile1 = read1, readfile2 = read2, output_file = outBAM,
-      nthreads = 2, type = 1, unique = TRUE, maxFragLength = 2000)
+#align(indexForSubread, readfile1 = read1, readfile2 = read2, output_file = outBAM,
+#      nthreads = 2, type = 1, unique = TRUE, maxFragLength = 2000)
 
 #----------------------------------------------------------------------------------------
 
@@ -53,7 +57,7 @@ align(indexForSubread, readfile1 = read1, readfile2 = read2, output_file = outBA
 #Output: Sorted_ATAC.bam
 
 #make an output path for the sorted file
-#sortedBAM <- "/lab/solexa_lehmann/Dros-ATAC-seq-analysis/Sorted_ATAC.bam"
+sortedBAM <- "/lab/solexa_lehmann/Dros-ATAC-seq-analysis/Sorted_ATAC.bam"
 
 #sort bam file
 #sortBam(outBAM, gsub("\\.bam", "", basename(sortedBAM)))
@@ -98,10 +102,10 @@ align(indexForSubread, readfile1 = read1, readfile2 = read2, output_file = outBA
 #b. Output file: Sorted_postqc_ATAC.bam
 
 #Set file path for the output file post quality control 
-#outBAM_postqc <- "/lab/solexa_lehmann/Dros-ATAC-seq-analysis/noDups.noMito.bam"
+outBAM_postqc <- "/lab/solexa_lehmann/Dros-ATAC-seq-analysis/ATAC.bam"
 
 #Set file path for the post quality control sorted file
-#sortedBAM_postqc <- "/lab/solexa_lehmann/Dros-ATAC-seq-analysis/Sorted_postqc_ATAC.bam"
+sortedBAM_postqc <- "/lab/solexa_lehmann/Dros-ATAC-seq-analysis/Sorted_ATAC.bam"
 
 #sort bam file
 #sortBam(outBAM_postqc, gsub("\\.bam", "", basename(sortedBAM_postqc)))
@@ -120,12 +124,12 @@ align(indexForSubread, readfile1 = read1, readfile2 = read2, output_file = outBA
 
 #Step 9: Plot the distribution of mapped reads. 
 
-#pdf(file="/lab/solexa_lehmann/Dros-ATAC-seq-analysis/dist_mapped_reads.pdf", width = 10, height = 10)
+pdf(file="/lab/solexa_lehmann/Dros-ATAC-seq-analysis/dist_mapped_reads_0.3xTn5.pdf", width = 10, height = 10)
 
-#idxstatsBam(sortedBAM_postqc) %>% ggplot(aes(seqnames, mapped, fill = seqnames)) +
-#  geom_bar(stat = "identity") + coord_flip()
+idxstatsBam(sortedBAM_postqc) %>% ggplot(aes(seqnames, mapped, fill = seqnames)) +
+  geom_bar(stat = "identity") + coord_flip()
 
-#dev.off()
+dev.off()
 
 #----------------------------------------------------------------------------------------
 
@@ -136,23 +140,28 @@ align(indexForSubread, readfile1 = read1, readfile2 = read2, output_file = outBA
 #    flag = scanBamFlag(isPaired = TRUE, isProperPair = TRUE), what = c("qname", 
 #        "mapq", "isize")))
 
-#atacReads_read1 <- GenomicAlignments::first(atacReads)
-#insertSizes <- abs(elementMetadata(atacReads_read1)$isize)
+#Only plotting reads that mapped to Chromosome 20
+atacReads <- readGAlignmentPairs(sortedBAM_postqc, param = ScanBamParam(mapqFilter = 1, 
+    flag = scanBamFlag(isPaired = TRUE, isProperPair = TRUE), what = c("qname", 
+        "mapq", "isize")))
+
+atacReads_read1 <- GenomicAlignments::first(atacReads)
+insertSizes <- abs(elementMetadata(atacReads_read1)$isize)
 
 #plot fragment length with ggplot
-#fragLenPlot <- table(insertSizes) %>% data.frame %>% rename(InsertSize = insertSizes,
-#    Count = Freq) %>% mutate(InsertSize = as.numeric(as.vector(InsertSize)),
-#    Count = as.numeric(as.vector(Count))) %>% ggplot(aes(x = InsertSize, y = Count)) +
-#    geom_line()
+fragLenPlot <- table(insertSizes) %>% data.frame %>% rename(InsertSize = insertSizes,
+    Count = Freq) %>% mutate(InsertSize = as.numeric(as.vector(InsertSize)),
+    Count = as.numeric(as.vector(Count))) %>% ggplot(aes(x = InsertSize, y = Count)) +
+    geom_line()
 
-#pdf(file="/lab/solexa_lehmann/Dros-ATAC-seq-analysis/fragment_lengths.pdf", width = 5, height = 5)
-#fragLenPlot + theme_bw()
-#dev.off()
+pdf(file="/lab/solexa_lehmann/Dros-ATAC-seq-analysis/fragment_lengths_0.3xTn5.pdf", width = 5, height = 5)
+fragLenPlot + theme_bw() + xlim(0,2000) + ylim(0,3000)
+dev.off()
 
 #Fragment length plot with y axis in log scale
-#pdf(file="/lab/solexa_lehmann/Dros-ATAC-seq-analysis/fragment_lengths_ylog.pdf", width = 5, height = 5)
+pdf(file="/lab/solexa_lehmann/Dros-ATAC-seq-analysis/fragment_lengths_ylog_0.3xTn5.pdf", width = 5, height = 5)
 #plot on a log scale
-#fragLenPlot + scale_y_continuous(trans = "log2") + theme_bw()
-#dev.off()
+fragLenPlot + scale_y_continuous(trans = "log2") + theme_bw()
+dev.off()
 
 #----------------------------------------------------------------------------------------
